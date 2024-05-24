55d99fc8f2a9fb3087bb300fb2e1cb3ffb39de37
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-05-24 19:30:08 +0530

    Added humidity dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Dashboard from './components/Dashboard'
 import MedicinesPage from './pages/Medicines';
+import HumidityDashboard from './pages/HumidityDashboard';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard/>} />
         <Route path="/medicines" element={<MedicinesPage />} />
+        <Route path='/humidity' element={<HumidityDashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
